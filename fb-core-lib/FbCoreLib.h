8786885ff622f8727e61ec4c8e335cf3f2822fee
Dmitry Blyakhman <33543531+dblyakhman@users.noreply.github.com>
2021-09-05 11:02:38 +0500

    Create FbCoreLib.h

---
+++ fb-core-lib/FbCoreLib.h
@@ -0,0 +1,13 @@
+#ifndef SYS_H
+#define SYS_H
+
+//------------------------------------------------------------------------------------------------ 
+// файл sys.h 
+//------------------------------------------------------------------------------------------------ 
+#include "FuncBlock.h"
+#include "ThreadPool.h"
+#include "MultiThreadFBExecutor.h"
+#include "FBTimerType.h"
+
+#endif /* SYS_H */
+
